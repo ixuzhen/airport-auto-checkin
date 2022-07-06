85854a986bde71bf223c5d1b37c07e46ac33c4e6
xz <471340101@qq.com>
2022-07-06 19:00:10 +0800

    加入log文件

--- main.py
+++ main.py
@@ -26,43 +26,48 @@ def get_yaml_data(yaml_file):
 
 
 if __name__ == '__main__':
-    # 设置为无头
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument('--headless')
-    # 安装驱动
-    service = Service(executable_path=ChromeDriverManager().install())
-    # 加载配置
-    config = get_yaml_data('./config.yaml')
-    website = config["website"]
-    user_name = config["user_name"]
-    password = config['password']
-    code_location = config['codelocation']
+    try:
+        # 设置为无头
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument('--headless')
+        # 安装驱动
+        service = Service(executable_path=ChromeDriverManager().install())
+        # 加载配置
+        config = get_yaml_data('./config.yaml')
+        website = config["website"]
+        user_name = config["user_name"]
+        password = config['password']
+        code_location = config['codelocation']
 
-    # 创建浏览器
-    driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
-    driver.maximize_window()
-    driver.get(website)
-    # driver.implicitly_wait(3000)
-    time.sleep(5)
-    driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(user_name)
-    driver.find_element(By.XPATH, '//*[@id="passwd"]').send_keys(password)
-    time.sleep(5)
-    driver.find_element(By.XPATH, '//*[@id="login"]').click()
-    time.sleep(30)
-    # driver.find_element(By.XPATH,'//*[@id="ui_menu"]/div/div/ul/li[1]').click()
-    # time.sleep(5)
-    with open(os.path.join(code_location, 'log.txt'), 'a') as log:
+        # 创建浏览器
+        driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
+        driver.maximize_window()
+        driver.get(website)
+        # driver.implicitly_wait(3000)
+        time.sleep(5)
+        driver.find_element(By.XPATH, '//*[@id="email"]').send_keys(user_name)
+        driver.find_element(By.XPATH, '//*[@id="passwd"]').send_keys(password)
+        time.sleep(5)
+        driver.find_element(By.XPATH, '//*[@id="login"]').click()
+        time.sleep(30)
+        with open(os.path.join(code_location, 'log.txt'), 'a') as log:
 
-        if is_exits_element(By.XPATH, '//*[@id="checkin"]'):
-            driver.find_element(By.XPATH,'//*[@id="checkin"]').click()
-            time.sleep(30)
-            driver.find_element(By.XPATH, '//*[@id="result_ok"]').click()
-            messege = driver.find_element(By.XPATH,'//*[@id="checkin-msg"]').text
-            print(messege)
-            log.write(messege)
-            log.write('\n')
-        else:
-            print('签到失败')
-            log.write('签到失败')
-            log.write('\n')
-    driver.quit()
+            if is_exits_element(By.XPATH, '//*[@id="checkin"]'):
+                driver.find_element(By.XPATH,'//*[@id="checkin"]').click()
+                time.sleep(30)
+                driver.find_element(By.XPATH, '//*[@id="result_ok"]').click()
+                messege = driver.find_element(By.XPATH,'//*[@id="checkin-msg"]').text
+                print(messege)
+                log.write(messege)
+                log.write('\n')
+            else:
+                print('签到失败')
+                log.write('签到失败')
+                log.write('\n')
+        driver.quit()
+    except Exception as e:
+        with open('./error.txt', 'a') as error:
+            error.write(repr(e))
+            error.write('\n')
+            error.write(str(e))
+            error.write('\n')
