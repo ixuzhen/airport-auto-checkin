ef84b64609881f588167af1c246e5e3503b598da
xz <471340101@qq.com>
2022-07-07 17:17:34 +0800

    加入log文件

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+import argparse
 
 
 import time
@@ -26,18 +27,21 @@ def get_yaml_data(yaml_file):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file', type=str, default='config.yaml', help='配置文件的位置')
+    args = parser.parse_args()
     try:
+        PATH = os.getcwd()
         # 设置为无头
         chrome_options = ChromeOptions()
         chrome_options.add_argument('--headless')
         # 安装驱动
         service = Service(executable_path=ChromeDriverManager().install())
         # 加载配置
-        config = get_yaml_data('./config.yaml')
+        config = get_yaml_data(os.path.join(PATH, args.config_file))
         website = config["website"]
         user_name = config["user_name"]
         password = config['password']
-        code_location = config['codelocation']
 
         # 创建浏览器
         driver = webdriver.Chrome(service=service, chrome_options=chrome_options)
@@ -50,9 +54,10 @@ if __name__ == '__main__':
         time.sleep(5)
         driver.find_element(By.XPATH, '//*[@id="login"]').click()
         time.sleep(30)
-        with open(os.path.join(code_location, 'log.txt'), 'a') as log:
+        with open(os.path.join(PATH, 'log.txt'), 'a') as log:
 
             if is_exits_element(By.XPATH, '//*[@id="checkin"]'):
+                # messege = 'success'
                 driver.find_element(By.XPATH,'//*[@id="checkin"]').click()
                 time.sleep(30)
                 driver.find_element(By.XPATH, '//*[@id="result_ok"]').click()
@@ -66,8 +71,6 @@ if __name__ == '__main__':
                 log.write('\n')
         driver.quit()
     except Exception as e:
-        with open('./error.txt', 'a') as error:
-            error.write(repr(e))
-            error.write('\n')
+        with open(os.path.join(PATH, 'error.txt'), 'a') as error:
             error.write(str(e))
             error.write('\n')
